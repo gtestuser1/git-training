79d1e004c8fd8e35f8a1222294d3010e9487186b
Prudhvi A <adharapurapu.prudhvi@wipro.com>
2024-09-13 05:40:04 +0000

    added comment and changed the count value

--- apythonfile.py
+++ apythonfile.py
@@ -1,5 +1,9 @@
 #To Ping the google.com 5 times
 import os
 
+<<<<<<< HEAD
 os.system("ping google.com -c 5")
+=======
+os.system("ping google.com -n 6")
+>>>>>>> dc60a48 (added comment and changed the count)
 print("Ping complete")
